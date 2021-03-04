2f97bd51f48041a4e8ac9044f90eedb9e40f6148
liqinchun <liqc@inhand.com.cn>
2021-03-04 18:57:07 +0800

    first commit

---
+++ src/main/java/com/liqinchun/knowledge/parent/KnowledgeParentApplication.java
@@ -0,0 +1,13 @@
+package com.liqinchun.knowledge.parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeParentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liqinchun/knowledge/parent/KnowledgeParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liqinchun.knowledge.parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
